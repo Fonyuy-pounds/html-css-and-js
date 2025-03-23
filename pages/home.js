5e50e9f2bb7a2507531f531c3e0c071dc03c32e6
Fonyuy-pounds <fonyuypounds@gmail.com>
2025-03-23 14:57:25 +0100

    weekend3

--- pages/home.js
+++ pages/home.js
@@ -12,11 +12,26 @@
 const getstart = document.getElementById("getstarted");
 const popup = document.getElementById("poppupcontainer");
 const closeBtn = document.getElementById("close");
+const form =document.getElementById("formcontainer")
 
 console.log(getstart);
 
 getstart.addEventListener("click", function() {
     popup.style.display = "flex"; // Corrected from `direction` to `display`
 });
+ closeBtn.addEventListener('click', function() {
+    popup.style.display="none"
+ })
+   
 
- 
+ form.addEventListener('submit', function() {
+    const password = document.getElementById('password').value
+    // console.log(" this is my password").value
+    const confirmPassword=document.getElementById('confirm-password').value
+    if(password !==confirmPassword){
+        alert("password does not match")
+        return
+    }
+
+    
+ })
